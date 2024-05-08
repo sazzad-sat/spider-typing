ef001029ff753a1fc73713bd67ae7f7cb0242aaa
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-05-08 13:10:23 +0600

    change performance table

--- src/db/schema/typingPerformance.ts
+++ src/db/schema/typingPerformance.ts
@@ -1,15 +1,26 @@
-import { doublePrecision, jsonb, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
-import { type KeyboardKeys } from './enums'
+import { time, pgTable, serial, text, timestamp, unique, date } from 'drizzle-orm/pg-core'
+import { keyboardKeys } from './enums'
+import { sql } from 'drizzle-orm'
 
-export const typingPerformance = pgTable('typing_performance', {
-  id: serial('id').primaryKey(),
-  practiceDate: timestamp('practice_date', { mode: 'date' }).notNull(),
-  duration: doublePrecision('duration').notNull(),
-  problemKeys: jsonb('problem_keys').$type<KeyboardKeys[]>().default([]).notNull(),
-  userId: text('user_id').notNull(),
-  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
-  updatedAt: timestamp('updated_at', { mode: 'date' })
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
-})
+export const typingPerformance = pgTable(
+  'typing_performance',
+  {
+    id: serial('id').primaryKey(),
+    practiceDate: date('practice_date', { mode: 'date' }).notNull(),
+    startTime: time('start_time').notNull(),
+    endTime: time('end_time').notNull(),
+    problemKeys: keyboardKeys('problem_keys')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    userId: text('user_id').notNull(),
+    createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { mode: 'date' })
+      .defaultNow()
+      .$onUpdate(() => new Date())
+      .notNull(),
+  },
+  (t) => ({
+    uniqueRecord: unique().on(t.practiceDate, t.startTime, t.endTime, t.userId),
+  })
+)
